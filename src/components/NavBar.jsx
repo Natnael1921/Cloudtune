a2e7e3463741ba02185c76a0fc6852156ee554b2
Natnael Mekonnen <nnatnaelmekonnen19@gmail.com>
2026-02-19 01:15:24 +0300

    fix: resolve react build error and reinstall dependencies

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -24,6 +24,7 @@ export function NavBar({ query, setQuery, topArtist }) {
       </div>
 
       <div className={`nav ${menuOpen ? "nav-open" : ""}`}>
+        <NavLink to="/">Home</NavLink>
         <NavLink to="/liked" onClick={() => setMenuOpen(false)}>
           Liked
         </NavLink>
--- src/pages/PlayList.jsx
+++ src/pages/PlayList.jsx
@@ -4,7 +4,7 @@ import { NavBar } from '../components/NavBar'
 export default function PlayList() {
   return (
     <div>
-        <NavBar/>
+        <NavBar topArtist={[]}/>
       <h2 className='playlist'>Working on this feature ...</h2>
     </div>
   )
